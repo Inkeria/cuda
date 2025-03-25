4e0d75d2c278cecd7c08198d880cbac43e97008e
Inkeria <1375147168@qq.com>
2025-03-25 17:26:40 +0800

    1

--- Mat.cu
+++ Mat.cu
@@ -1,9 +1,16 @@
-#include<bits/stdc++.h>
+#include<stdio.h>
+#include<cuda.h>
 
-using namespace std;
-
+__global void printHello()
+{
+    int index = threadIdx.x + blockIdx.x * blockDim.x;
+    printf("Hello GPU by thread:%d",index);
+}
 int main()
 {
-    
+    dim3 grid_dim = {1, 1, 1};
+    dim3 block_dim = {4, 1, 1};
+    printHello<<<grid_dim, block_dim>>>();
+    cudaDeviceSynchronize();
     return 0;
 }
