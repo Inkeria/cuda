e568eb25c357782800e5bfd5ac37cb425d8de8eb
Inkeria <1375147168@qq.com>
2025-03-26 16:08:03 +0800

    1

--- reduce.cu
+++ reduce.cu
@@ -33,7 +33,7 @@ int main()
     A = (float*) malloc(n * sizeof(float));
     ans = (float *) malloc(sizeof(float));
     for(int i = 0;i < n; ++i){
-        A[i] = (n % (i + 1)) * 1e-1; 
+        A[i] = i * 1e-1; 
     }
     float *dA;
     cudaMalloc((void **)&dA, n * sizeof(float));
