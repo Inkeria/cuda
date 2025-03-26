bd29c064daccc88e8c1ff38aa517e91173b42902
Inkeria <1375147168@qq.com>
2025-03-26 16:12:47 +0800

    1

--- reduce.cu
+++ reduce.cu
@@ -33,16 +33,16 @@ int main()
     A = (float*) malloc(n * sizeof(float));
     ans = (float *) malloc(sizeof(float));
     for(int i = 0;i < n; ++i){
-        A[i] = (n - i + 1) * 1e-1; 
+        A[i] = (n % (i + 1)) * 1e-2;
     }
     float *dA;
     cudaMalloc((void **)&dA, n * sizeof(float));
     printf("%f\n",A[0]);
     cudaMemcpy(A, dA, n * sizeof(float), cudaMemcpyHostToDevice);
 
-    // dim3 grid_dim(1, 1, 1);
-    // dim3 block_dim(1024, 1, 1);
-    // reduce<1024><<<grid_dim, block_dim>>>(dA, n);
+    dim3 grid_dim(1, 1, 1);
+    dim3 block_dim(1024, 1, 1);
+    reduce<1024><<<grid_dim, block_dim>>>(dA, n);
 
     cudaMemcpy(dA, A, n * sizeof(float), cudaMemcpyDeviceToHost);
     printf("%f\n",A[0]);
